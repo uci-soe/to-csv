7247e4b706120f469178985cd2d5d5bc616ce152
Rhett Lowe <rng2ml@gmail.com>
2015-09-14 13:38:06 -0700

    updating tests, adding sample files with sample names and numbers

--- test/index.js
+++ test/index.js
@@ -1,10 +1,25 @@
 'use strict';
 
 var assert = require('assert');
-var convertXls = require('../lib');
+var convert = require('../lib');
 
 describe('convert-xls', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  it('should output as CSV.', function () {
+    assert(false, 'Not yet implemented.');
+  });
+  it('should output as JSON.', function () {
+    assert(false, 'Not yet implemented.');
+  });
+  it('should output as accept files as multi-part form.', function () {
+    assert(false, 'Not yet implemented.');
+  });
+  it('should accept buffers/streams.', function () {
+    assert(false, 'Not yet implemented.');
+  });
+  it('should remove header/meta data from top of document.', function () {
+    assert(false, 'Not yet implemented.');
+  });
+  it('should identify if meta/header data is in top of document', function () {
+    assert(false, 'Not yet implemented.');
   });
 });
