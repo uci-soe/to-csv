601724666a3e74cbb19e9aa241014bd6c39eaaa0
Rhett Lowe <rng2ml@gmail.com>
2015-09-14 17:01:48 -0700

    Changed nesting of describes

--- test/error.js
+++ test/error.js
@@ -1,68 +1,74 @@
 var assert = require('assert');
-var error = require('../lib/error');
+var error  = require('../lib/error');
 
-describe('Error Package, ifNot', function () {
-  it("should throw error when given as first argument", function () {
-    assert.throws(function() {
-      error.ifNot(new Error());
-    }, Error);
-  });
-  it("should throw error when first argument false", function () {
-    assert.throws(function() {
-      error.ifNot(false);
-    }, Error);
-  });
-  it("should not throw error when first argument true", function () {
-    assert.doesNotThrow(function() {
-      error.ifNot(true);
-    });
-  });
-  it("should throw error when first argument falsey", function () {
-    assert.throws(function() {
-      error.ifNot(null);
-    }, Error);
-  });
-  it("should not throw error when first argument truthy", function () {
-    assert.doesNotThrow(function() {
-      error.ifNot('test');
-    });
-  });
-  it("puts message in error when first argument falsey and second string", function () {
-    assert.throws(function() {
-      error.ifNot(false, "testing");
-    }, /testing/);
-  });
-});
+describe('Error Package', function () {
 
-describe('Error Package, if', function () {
-  it("should throw error when given as first argument", function () {
-    assert.throws(function() {
-      error.if(new Error());
-    }, Error);
-  });
-  it("should throw error when first argument true", function () {
-    assert.throws(function() {
-      error.if(true);
-    }, Error);
-  });
-  it("should throw error when first argument truthy", function () {
-    assert.throws(function() {
-      error.if('testing');
-    }, Error);
-  });
-  it("should not throw error when first argument false", function () {
-    assert.doesNotThrow(function() {
-      error.if(false);
+  describe('if', function () {
+    it("should throw error when given as first argument", function () {
+      assert.throws(function () {
+        error.if(new Error());
+      }, Error);
+    });
+    it("should throw error when first argument true", function () {
+      assert.throws(function () {
+        error.if(true);
+      }, Error);
+    });
+    it("should throw error when first argument truthy", function () {
+      assert.throws(function () {
+        error.if('testing');
+      }, Error);
+    });
+    it("should not throw error when first argument false", function () {
+      assert.doesNotThrow(function () {
+        error.if(false);
+      });
+    });
+    it("should throw error when first argument falsey", function () {
+      assert.doesNotThrow(function () {
+        error.if(null);
+      });
+    });
+    it("puts message in error when first argument truthy and second string", function () {
+      assert.throws(function () {
+        error.if(true, "testing");
+      }, /testing/);
     });
   });
-  it("should throw error when first argument falsey", function () {
-    assert.doesNotThrow(function() {
-      error.if(null);
+
+
+
+  describe('ifNot', function () {
+    it("should throw error when given as first argument", function () {
+      assert.throws(function () {
+        error.ifNot(new Error());
+      }, Error);
+    });
+    it("should throw error when first argument false", function () {
+      assert.throws(function () {
+        error.ifNot(false);
+      }, Error);
+    });
+    it("should not throw error when first argument true", function () {
+      assert.doesNotThrow(function () {
+        error.ifNot(true);
+      });
+    });
+    it("should throw error when first argument falsey", function () {
+      assert.throws(function () {
+        error.ifNot(null);
+      }, Error);
+    });
+    it("should not throw error when first argument truthy", function () {
+      assert.doesNotThrow(function () {
+        error.ifNot('test');
+      });
+    });
+    it("puts message in error when first argument falsey and second string", function () {
+      assert.throws(function () {
+        error.ifNot(false, "testing");
+      }, /testing/);
     });
   });
-  it("puts message in error when first argument truthy and second string", function () {
-    assert.throws(function() {
-      error.if(true, "testing");
-    }, /testing/);
-  });
+
 });
